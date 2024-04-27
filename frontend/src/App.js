26e1b6be9d9d511c5909af46cc75960a29ad20de
Nishi <humayuranishi@gmail.com>
2024-04-27 13:34:12 -0400

    bruh

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,22 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Layout from './Components/Layout'
-import Navbar from './Components/Navbar'
-import Home from './Pages/Home'
-import ModuleList from './Pages/ModuleList'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Components/Layout';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import ModuleList from './Pages/ModuleList';
 
 function App() {
   return (
     <div>
-      <div className='flex h-screen flex-col'>
-        <Home />
-      </div>
-
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path='modules' element={<ModuleList />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path='/' element={<Layout />}>
-    //       <Route index element={<Home />} />
-    //       <Route path='modules' element={<ModuleList />} />
-    //     </Route>
-    //   </Routes>
-    // </BrowserRouter>
   );
 }
 
--- frontend/src/Pages/Home/index.jsx
+++ frontend/src/Pages/Home/index.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import Navbar from '../../Components/Navbar';
-import logo from '../../Components/Assets/logo-large.png';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import Navbar from '../../Components/Navbar'
+import logo from '../../Components/Assets/logo-large.png'
+// import { Link } from 'react-router-dom';
 
 import './style.css';
 
 export const index = () => {
     return (
         <>
-            <Navbar />
             <div className='h-full w-full'>
                 <div className='flex flex-col'>
                     <div className='flex flex-row'>
@@ -17,8 +16,8 @@ export const index = () => {
                     </div>
                     <p className=''>Future of pain managements. Way towards a better life.</p>
                     <div className='flex flex-row'>
-                        <Link to='' className>what</Link>
-                        <Link to='' className></Link>
+                        {/* <Link to='' className>what</Link>
+                        <Link to='' className></Link> */}
                     </div>
                 </div>
             </div>
