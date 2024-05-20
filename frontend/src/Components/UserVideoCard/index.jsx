0264b4e7f3735fafac62a7b8d1a3857408127948
Nishi <humayuranishi@gmail.com>
2024-05-20 03:52:56 -0400

    user modules complete

---
+++ frontend/src/Components/UserVideoCard/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Index = ({ lesson, title, module, setVideo, video }) => {
+    return (
+        <>
+            {video === -1 ?
+                <Link className='flex flex-row w-full items-start gap-6' onClick={() => { setVideo(lesson) }} to={`/user/module/${module}/video/${lesson}/`}>
+                    <div className='bg-[#D9D9D9] w-[240px] h-[135px]' />
+                    <div className='flex flex-col items-start'>
+                        <p className='poppins-bold text-xl'>LESSON {lesson}:</p>
+                        <p className='poppins-regular text-base'>{title}</p>
+                    </div>
+                </Link>
+                :
+                <Link className='flex flex-col gap-1' onClick={() => { setVideo(lesson) }} to={`/user/module/${module}/video/${lesson}/`}>
+                    <div className='bg-[#D9D9D9] w-[263px] h-[148px] flex-shrink-0' />
+                    <div className='flex flex-col items-start'>
+                        <p className='poppins-semibold text-lg'>LESSON {lesson}:</p>
+                        <p className='poppins-regular text-xs'>{title}</p>
+                    </div>
+                </Link>
+            }
+        </>
+    )
+}
+
+export default Index
--- frontend/src/Pages/UserModules/index.jsx
+++ frontend/src/Pages/UserModules/index.jsx
@@ -3,6 +3,8 @@ import UserModuleCard from '../../Components/UserModuleViewCard';
 import { UilAngleLeft, UilLinkH } from '@iconscout/react-unicons';
 import './style.css';
 import { Link, useParams } from 'react-router-dom';
+import UserVideoCard from '../../Components/UserVideoCard';
+import Comments from '../../Components/Comments';
 
 import document from '../../Assets/Documents/May_2024_Resume.pdf';
 
@@ -10,6 +12,7 @@ const Index = () => {
     const { moduleID, videoID } = useParams();
     const [module, setModule] = useState(-1);
     const [video, setVideo] = useState(-1);
+    const [videoTitle, setVideoTitle] = useState("Neck & Shoulder, Shoulder Joint");
 
     const [category, setCategory] = useState("Neck & Shoulder, Shoulder Joint");
     const [description, setDescription] = useState("Treatment covers a wide range of conditions including general neck and shoulder pain, muscle cramps, and sprains. It also addresses cervical disk issues, straight neck syndrome, turtle neck syndrome, headaches, and numbness or tingling sensations in the arms and hands. Additional focuses include Impingement Syndrome, Frozen Shoulder, and Rotator Cuff tears.");
@@ -51,7 +54,30 @@ const Index = () => {
                                 <UilAngleLeft color='#505050' />
                                 <p className='poppins-bold text-[#505050] text-base'>Back</p>
                             </Link>
-                            <div className='flex h-full w-full'> </div>
+                            <div className='h-full w-full flex flex-row overflow-hidden'>
+                                <div className='flex flex-col gap-3 overflow-y-scroll custom-scrollbar border-r-2 border-[#9F9F9F] pl-4 pr-12 flex-shrink-0 h-auto'>
+                                    <div className='w-[1104px] h-[621px] bg-[#D9D9D9] flex-shrink-0' />
+                                    <div className='flex flex-row items-center gap-2 mb-2'>
+                                        <p className='poppins-bold text-[30px]'>LESSON {video}:</p>
+                                        <p className='poppins-semibold text-[30px]'>{videoTitle}</p>
+                                    </div>
+                                    <Comments module={module} video={video} />
+                                </div>
+                                <div className='h-full w-full flex flex-col gap-8 overflow-y-scroll custom-scrollbar items-center mr-[-32px]'>
+                                    <UserVideoCard lesson={1} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                    <UserVideoCard lesson={2} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                    <UserVideoCard lesson={3} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                    <UserVideoCard lesson={4} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                    <UserVideoCard lesson={5} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                    <UserVideoCard lesson={6} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                    <UserVideoCard lesson={7} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                    <UserVideoCard lesson={8} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                    <UserVideoCard lesson={9} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                    <UserVideoCard lesson={10} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                    <UserVideoCard lesson={11} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                    <UserVideoCard lesson={12} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                </div>
+                            </div>
                         </div>
                         :
                         <div className='flex flex-col overflow-hidden h-full'>
@@ -88,25 +114,24 @@ const Index = () => {
                                         </div>
                                     </div>
                                 </div>
-                                {/* <div className='h-full w-full flex flex-col px-8 overflow-hidden'>
+                                <div className='h-full w-full flex flex-col px-8 overflow-hidden'>
                                     <div className='h-full w-full flex flex-col gap-8 overflow-y-scroll custom-scrollbar'>
-                                        <AdminVideoCard lesson={1} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
-                                        <AdminVideoCard lesson={2} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
-                                        <AdminVideoCard lesson={3} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
-                                        <AdminVideoCard lesson={4} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
-                                        <AdminVideoCard lesson={5} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
-                                        <AdminVideoCard lesson={6} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
-                                        <AdminVideoCard lesson={7} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
-                                        <AdminVideoCard lesson={8} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
-                                        <AdminVideoCard lesson={9} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
-                                        <AdminVideoCard lesson={10} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
-                                        <AdminVideoCard lesson={11} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
-                                        <AdminVideoCard lesson={12} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                        <UserVideoCard lesson={1} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                        <UserVideoCard lesson={2} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                        <UserVideoCard lesson={3} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                        <UserVideoCard lesson={4} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                        <UserVideoCard lesson={5} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                        <UserVideoCard lesson={6} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                        <UserVideoCard lesson={7} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                        <UserVideoCard lesson={8} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                        <UserVideoCard lesson={9} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                        <UserVideoCard lesson={10} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                        <UserVideoCard lesson={11} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
+                                        <UserVideoCard lesson={12} title='Neck & Shoulder, Shoulder Joint' module={module} setVideo={setVideo} video={video} />
                                     </div>
-                                </div> */}
+                                </div>
                             </div>
                         </div>
-
                     }
                 </div>
             }
