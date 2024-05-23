c697d48a0b1aad9e17afae0f50a429c7c89e1717
hackerlava <basnetsan25@gmail.com>
2024-05-22 23:08:02 -0400

    tiny change

--- backend/routes/oauth.js
+++ backend/routes/oauth.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const dotenv = require("dotenv");
+const dotenv = require("../dotenv");
 dotenv.config();
 const {OAuth2Client} = require('google-auth-library');
 var google = require('googleapis').google;
--- backend/vimeotest.js
+++ backend/vimeotest.js
@@ -3,10 +3,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-console.log(process.env.VIMEO_CLIENTID)
-console.log(process.env.VIMEO_CLIENTSECRET)
-console.log(process.env.VIMEO_ACCESS_TOKEN)
-
 let Vimeo = require('vimeo').Vimeo;
 let client = new Vimeo(process.env.VIMEO_CLIENTID, process.env.VIMEO_CLIENTSECRET, process.env.VIMEO_ACCESS_TOKEN);
 
@@ -20,4 +16,35 @@ client.request({
     }
 
     console.log(body);
+  })
+
+let file_name = "C:\\Users\\San Basnet\\Downloads\\d793f97348274cbaa7892633b7376664.mov"
+// client.upload(
+//   file_name,
+//   {
+//     'name': 'test_video',
+//     'description': 'The description goes here.'
+//   },
+//   function (uri) {
+//     console.log('Your video URI is: ' + uri);
+//   },
+//   function (bytes_uploaded, bytes_total) {
+//     var percentage = (bytes_uploaded / bytes_total * 100).toFixed(2)
+//     console.log(bytes_uploaded, bytes_total, percentage + '%')
+//   },
+//   function (error) {
+//     console.log('Failed because: ' + error)
+//   }
+// )
+
+let uri = '/videos/949383072';
+
+client.request(uri + '?fields=link', function (error, body, statusCode, headers) {
+    if (error) {
+      console.log('There was an error making the request.')
+      console.log('Server reported: ' + error)
+      return
+    }
+  
+    console.log('Your video link is: ' + body.link)
   })
