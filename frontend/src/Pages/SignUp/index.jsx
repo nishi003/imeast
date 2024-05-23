a44aa5577792bcde0c29cb242b889cfde0cb3514
Nishi <humayuranishi@gmail.com>
2024-05-22 23:11:27 -0400

    meh

--- frontend/src/Pages/SignUp/index.jsx
+++ frontend/src/Pages/SignUp/index.jsx
@@ -22,6 +22,7 @@ const Index = () => {
     const [location, setLocation] = useState("");
     const [profession, setProfession] = useState("");
     const [period, setPeriod] = useState("");
+    const [other, setOther] = useState(null);
 
     const [sectionOne, setSectionOne] = useState(false);
     const [sectionTwo, setSectionTwo] = useState(false);
@@ -32,6 +33,7 @@ const Index = () => {
         first_name: null,
         last_name: null,
         sex: null,
+        birthDate: null,
         email: null,
         phone_number: null,
         password1: null,
@@ -48,69 +50,26 @@ const Index = () => {
         setErrors({});
     }, [navigate]);
 
-    const handleFirstNameChange = ((event) => {
-        const newFirstName = event.target.value;
-        setFirstName(newFirstName);
+    const [formData, setFormData] = useState({
+        first_name: null,
+        last_name: null,
+        sex: null,
+        birthDate: null,
+        email: null,
+        phone_number: null,
+        password1: null,
+        password2: null,
+        college: null,
+        license: null,
+        address: null,
+        profession: null,
+        period: null,
+        other: null
     });
 
-    const handleLastNameChange = (event) => {
-        const newLastName = event.target.value;
-        setLastName(newLastName);
-    };
-
-    const handelSexChange = ((event) => {
-        const newSex = event.target.value;
-        setSex(newSex);
-    });
-
-    const handleBirthdayChange = ((event) => {
-        const newBirthday = event.target.value;
-        setBirthday(newBirthday);
-    })
-
-    const handleEmailChange = (event) => {
-        const newEmail = event.target.value;
-        setEmail(newEmail);
-    };
-
-    const handlePhoneNumberChange = (event) => {
-        const newPhoneNumber = event.target.value;
-        setPhoneNnmber(newPhoneNumber);
-    }
-
-    const handlePassword1Change = ((event) => {
-        const newPassword1 = event.target.value;
-        setPassword1(newPassword1);
-    });
-
-    const handlePassword2Change = ((event) => {
-        const newPassword2 = event.target.value;
-        setPassword2(newPassword2);
-    });
-
-    const handleCollegeChange = ((event) => {
-        const newCollege = event.target.value;
-        setCollege(newCollege);
-    });
-
-    const handleLicenseChange = (event) => {
-        const newLicense = event.target.value;
-        setLicense(newLicense);
-    };
-
-    const handleLocationChange = (event) => {
-        const newLocation = event.target.value;
-        setLocation(newLocation);
-    };
-
-    const handleProfessionChange = (event) => {
-        const newProfession = event.target.value;
-        setProfession(newProfession);
-    };
-
-    const handlePeriodChange = (event) => {
-        const newPeriod = event.target.value;
-        setPeriod(newPeriod);
+    const handleInputChangeForm = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
     };
 
     useEffect(() => {
@@ -173,17 +132,17 @@ const Index = () => {
                             <div className='flex flex-row gap-5'>
                                 <div className='flex flex-col gap-1'>
                                     <label htmlFor='first-name' className='poppins-semibold text-[15px] text-[#767676] pl-2'>First Name</label>
-                                    <input type='text' id='first-name' value={firstName} onChange={handleFirstNameChange} required className='h-[58px] w-[326px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Fill in your first name' />
+                                    <input type='text' id='first-name' value={formData['first_name']} onChange={handleInputChangeForm} required className='h-[58px] w-[326px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Fill in your first name' />
                                 </div>
                                 <div className='flex flex-col gap-1'>
                                     <label htmlFor='last-name' className='poppins-semibold text-[15px] text-[#767676] pl-2'>Last Name</label>
-                                    <input type='text' id='last-name' value={lastName} onChange={handleLastNameChange} required className='h-[58px] w-[326px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Fill in your last name' />
+                                    <input type='text' id='last-name' value={formData['last_name']} onChange={handleInputChangeForm} required className='h-[58px] w-[326px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Fill in your last name' />
                                 </div>
                             </div>
                             <div className='flex flex-row gap-5'>
                                 <div className='flex flex-col gap-1'>
                                     <label className='poppins-semibold text-[15px] text-[#767676] pl-2'>Sex</label>
-                                    <select name="sex" value={sex} onChange={handelSexChange} className='h-[58px] w-[326px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]'>
+                                    <select name="sex" value={formData['sex']} onChange={handleInputChangeForm} className='h-[58px] w-[326px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]'>
                                         <option value="" disabled selected hidden>Select sex</option>
                                         <option value="male" className="text-[15px]">Male</option>
                                         <option value="female" className="text-[15px]">Female</option>
@@ -193,24 +152,24 @@ const Index = () => {
                                 </div>
                                 <div className='flex flex-col gap-1'>
                                     <label htmlFor='birthday' className='poppins-semibold text-[15px] text-[#767676] pl-2'>Birthday</label>
-                                    <input type='date' name='birthday' value={birthday} required onChange={handleBirthdayChange} className='h-[58px] w-[326px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' />
+                                    <input type='date' name='birthday' value={formData['birthDate']} required onChange={handleInputChangeForm} className='h-[58px] w-[326px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' />
                                 </div>
                             </div>
                             <div className='flex flex-col gap-1'>
                                 <label htmlFor='email' className='poppins-semibold text-[15px] text-[#767676] pl-2'>Email</label>
-                                <input type='email' name='email' value={email} required onChange={handleEmailChange} className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Type in your email here' />
+                                <input type='email' name='email' value={email} required onChange={handleInputChangeForm} className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Type in your email here' />
                             </div>
                             <div className='flex flex-col gap-1'>
                                 <label htmlFor='phoneNumber' className='poppins-semibold text-[15px] text-[#767676] pl-2'>Phone Number</label>
-                                <input type='text' name='phoneNumber' value={phoneNumber} required onChange={handlePhoneNumberChange} className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Type in your phone number here' />
+                                <input type='text' name='phoneNumber' value={phoneNumber} required onChange={handleInputChangeForm} className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Type in your phone number here' />
                             </div>
                             <div className='flex flex-col gap-1'>
                                 <label htmlFor='password1' className='poppins-semibold text-[15px] text-[#767676] pl-2'>Create Password</label>
-                                <input type='password' name='password1' value={password1} required onChange={handlePassword1Change} className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='●●●●●●●●' />
+                                <input type='password' name='password1' value={password1} required onChange={handleInputChangeForm} className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='●●●●●●●●' />
                             </div>
                             <div className='flex flex-col gap-1'>
                                 <label htmlFor='password2' className='poppins-semibold text-[15px] text-[#767676] pl-2'>Confirm Password</label>
-                                <input type='password' name='password2' value={password2} required onChange={handlePassword2Change} className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='●●●●●●●●' />
+                                <input type='password' name='password2' value={password2} required onChange={handleInputChangeForm} className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='●●●●●●●●' />
                             </div>
                         </div>
                     </div>
@@ -233,20 +192,20 @@ const Index = () => {
                         <div className='flex flex-col pl-5 gap-6 pb-20'>
                             <div className='flex flex-col gap-1'>
                                 <label htmlFor='college' className='poppins-semibold text-[15px] text-[#767676] pl-2'>Registered College</label>
-                                <input type='text' name='college' value={college} required onChange={handleCollegeChange} className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Type in your college' />
+                                <input type='text' name='college' value={college} required onChange={handleInputChangeForm} className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Type in your college' />
                             </div>
                             <div className='flex flex-col gap-1'>
                                 <label htmlFor='license' className='poppins-semibold text-[15px] text-[#767676] pl-2'>License Number</label>
-                                <input type='text' name='license' value={license} required onChange={handleLicenseChange} className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Type in your license number' />
+                                <input type='text' name='license' value={license} required onChange={handleInputChangeForm} className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Type in your license number' />
                             </div>
                             <div className='flex flex-col gap-1'>
                                 <label htmlFor='location' className='poppins-semibold text-[15px] text-[#767676] pl-2'>Location of practice (address)</label>
-                                <input type='text' name='location' value={location} required onChange={handleLocationChange} className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Type in your location of practice' />
+                                <input type='text' name='location' value={location} required onChange={handleInputChangeForm} className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Type in your location of practice' />
                             </div>
                             <div className='flex flex-row gap-4'>
                                 <div className='flex flex-col gap-1'>
                                     <label htmlFor='profession' className='poppins-semibold text-[15px] text-[#767676] pl-2'>Type of profession</label>
-                                    <select name='profession' value={profession} onChange={handleProfessionChange} className='h-[58px] w-[327px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]'>
+                                    <select name='profession' value={profession} onChange={handleInputChangeForm} className='h-[58px] w-[327px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]'>
                                         <option value="" disabled selected hidden>Select Profession</option>
                                         <option value='acupuncturist' className='text-[15px]'>Acupuncturist</option>
                                         <option value='physiologist' className='text-[15px]'>Physiologist</option>
@@ -256,7 +215,7 @@ const Index = () => {
                                 </div>
                                 <div className='flex flex-col gap-1'>
                                     <label htmlFor='period' className='poppins-semibold text-[15px] text-[#767676] pl-2'>Practicing period</label>
-                                    <select name='period' value={period} onChange={handlePeriodChange} className='h-[58px] w-[327px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]'>
+                                    <select name='period' value={period} onChange={handleInputChangeForm} className='h-[58px] w-[327px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]'>
                                         <option value="" disabled selected hidden>Select practicing period</option>
                                         <option value='lt1yr' className='text-[15px]'>Less than 1 year</option>
                                         <option value='1yr' className='text-[15px]'>1 year</option>
@@ -268,7 +227,7 @@ const Index = () => {
                             {profession === 'other' ?
                                 <div className='flex flex-col gap-1'>
                                     <label htmlFor='other-prof' className='poppins-semibold text-[15px] text-[#767676] pl-2'>Please Specify:</label>
-                                    <input type='text' name='other-prof' className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Type in your profession' />
+                                    <input type='text' name='other-prof' value={other} required onChange={handleInputChangeForm} className='h-[58px] w-[670px] border border-[#9F9F9F] rounded-[10px] text-[#9F9F9F] poppins-semibold px-3 focus:outline-[#767676]' placeholder='Type in your profession' />
                                 </div>
                                 :
                                 <></>
