98bd91c2c85d47e4cc2ad719beb8a4345c741b9d
Nishi <humayuranishi@gmail.com>
2024-05-25 18:17:27 -0400

    frontend minor changes

--- frontend/src/Components/Admin/AdminModuleCreate/index.jsx
+++ frontend/src/Components/Admin/AdminModuleCreate/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UilAngleLeft, UilLinkH, UilEditAlt } from '@iconscout/react-unicons';
+import FieldError from '../../../Components/FieldError';
 
 const Index = ({ setModule, moduleNumber }) => {
     const navigate = useNavigate();
@@ -38,14 +39,20 @@ const Index = ({ setModule, moduleNumber }) => {
                     <UilAngleLeft color='#505050' />
                     <p className='poppins-bold text-[#505050] text-base'>Back</p>
                 </Link>
-                <div className='h-full w-full flex flex-col overflow-hidden px-8 gap-4'>
+                <div className='h-full w-full flex flex-col overflow-hidden px-6 gap-4'>
                     <div className='w-full justify-start flex flex-col flex-shrink-0'>
                         <p className='poppins-medium text-black text-[40px]'>MODULE {moduleNumber}</p>
                     </div>
-                    <div className='h-full w-full px-8 gap-2 justify-center'>
+                    <div className='h-full w-full flex flex-col px-8 gap-3'>
                         <div className='flex flex-col gap-1'>
-                            <label htmlFor='title' className='poppins-semibold text-[15px] text-[#767676] pl-2'>Module Title</label>
+                            <label htmlFor='title' className='poppins-semibold text-[15px] text-[#767676] pl-2'>Title</label>
                             <input type='text' name='title' onChange={handleInputChangeForm} className={`w-full rounded-[10px] text-[#9F9F9F] poppins-semibold p-3 focus:outline-[#767676] ${errors['title'] === '' ? 'border border-[#9F9F9F]' : 'border-2 border-red-400'}`} placeholder='Type in the title of this module' />
+                            <FieldError fielderror={errors.title} />
+                        </div>
+                        <div className='flex flex-col gap-1'>
+                            <label htmlFor='description' className='poppins-semibold text-[15px] text-[#767676] pl-2'>Description</label>
+                            <textarea type='text' name='description' onChange={handleInputChangeForm} className={`w-full rounded-[10px] text-[#9F9F9F] poppins-semibold p-3 focus:outline-[#767676] resize-none ${errors['description'] === '' ? 'border border-[#9F9F9F]' : 'border-2 border-red-400'}`} placeholder='Type in the description of this module' rows={4} maxLength={500} />
+                            <FieldError fielderror={errors.description} />
                         </div>
                     </div>
                 </div>
