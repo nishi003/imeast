ccc12895705a55bd6d6f0d9a79b22540c118bf65
Nishi <humayuranishi@gmail.com>
2024-05-25 21:48:19 -0400

    backend nishi connection

--- backend/models/notifications.js
+++ backend/models/notifications.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
 
-//my own test database
-mongoose.connect("mongodb+srv://basnetsan25:InnWSc0E6O7SG3m6@cluster0.4vviipo.mongodb.net/");
-
 
 const notificationSchema = new mongoose.Schema({
-    username: {type: String, required: true},
+    username: { type: String, required: true },
     timeStamp: { type: Date, default: Date.now },
-    type: {type: String, required: true},
+    type: { type: String, required: true },
     readStatus: { type: String, default: 'unread' }, //read, unread
-    link: {type: String}
+    link: { type: String }
 });
 
 const Notification = mongoose.model('notifications', notificationSchema);
