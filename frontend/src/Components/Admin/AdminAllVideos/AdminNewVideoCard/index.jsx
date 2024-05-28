c9e7a518a373e969d6552b8b5db64b3d2863eec1
Nishi <humayuranishi@gmail.com>
2024-05-27 21:38:50 -0400

    um

--- frontend/src/Components/Admin/AdminAllVideos/AdminNewVideoCard/index.jsx
+++ frontend/src/Components/Admin/AdminAllVideos/AdminNewVideoCard/index.jsx
@@ -8,18 +8,21 @@ const Index = ({ moduleID, numLesson }) => {
     const navigate = useNavigate();
     const { lessonNumber, setLessonNumber } = useContext(LessonContext);
 
-    const handleCreate = (() => {
+    const handleCreate = () => {
         setLessonNumber(-1);
-        navigate(`/admin/module/${moduleID}/lesson/create/${numLesson}/`);
-    });
+        // navigate(`/admin/module/${moduleID}/lesson/create/${numLesson}/`);
+    };
 
     return (
-        <button className='flex flex-col gap-1' onClick={handleCreate}>
+        <Link className='flex flex-col gap-1' onClick={handleCreate} to={`/admin/module/${moduleID}/lesson/create/${numLesson}/`}>
             <div className={`bg-primary flex flex-col justify-center items-center flex-shrink-0 ${lessonNumber === -1 ? 'w-[240px] h-[135px]' : 'w-[263px] h-[148px]'}`}>
                 <UilPlusCircle color="#ffffff" size='40' />
                 <p className='text-white poppins-semibold text-2xl'>VIDEO</p>
             </div>
-        </button>
+            <div className='h-full w-full flex flex-col px-6 gap-2'>
+
+            </div>
+        </Link>
     )
 }
 
--- frontend/src/Pages/Admin/AdminLessonCreate/index.jsx
+++ frontend/src/Pages/Admin/AdminLessonCreate/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { UilAngleLeft } from '@iconscout/react-unicons';
 import { useNavigate, useParams } from 'react-router-dom';
@@ -13,6 +13,26 @@ const Index = () => {
         navigate(`/admin/module/${moduleID}/`);
     }
 
+    const [errors, setErrors] = useState({
+        title: '',
+        description: '',
+        thumbnail: '',
+        videoURL: '',
+        videoHTMLEmbed: '',
+    });
+
+    const [formData, setFormData] = useState({
+        title: '',
+        description: '',
+        thumbnail: '',
+        videoURL: '',
+        videoHTMLEmbed: '',
+    });
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+    };
+
     return (
         <div className='h-full w-full bg-[#DCDCDC] flex p-8 overflow-hidden'>
             <div className='w-full h-full rounded-[30px] bg-white flex flex-col flex-grow-0 flex-shrink-0 p-8 overflow-hidden gap-4'>
@@ -21,7 +41,10 @@ const Index = () => {
                     <p className='poppins-bold text-[#505050] text-base'>Back</p>
                 </button>
                 <div className='h-full w-full flex flex-col overflow-y-scroll px-6 gap-2 custom-scrollbar'>
-
+                    <div className='w-full flex flex-row flex-shrink-0 justify-between'>
+                        <p className='poppins-medium text-black text-[40px]'>LESSON {numLesson}</p>
+                        <button onClick={handleSubmit} className='py-3 px-8 bg-secondary poppins-semibold text-primary rounded-full hover:text-white hover:bg-primary hover:scale-105 hover:duration-200'>PUBLISH LESSON</button>
+                    </div>
                 </div>
             </div>
         </div>
